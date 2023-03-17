b73525c559c826ee5abde26ab7c96db4ae8fe882
Pranay Prasad <pranay.prasad@celllabs.com>
2023-03-17 18:11:48 +0530

    Update files in flutter folder

--- flutter/screens/splash_screen.dart
+++ flutter/screens/splash_screen.dart
@@ -1,14 +1,32 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () => Get.toNamed(homeScreen));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text('Splash Screen'),
+          child: Images.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.contain,
+            width: 200.0,
+            height: 200.0,
+          ),
         ),
       ),
     );
