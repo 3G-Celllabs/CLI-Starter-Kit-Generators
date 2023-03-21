4ca07e79af2f3cb7c9bf7f474730f31385590131
Pranay Prasad V <pranay.prasad@celllabs.com>
2023-03-21 17:10:57 +0000

    Update title in main.dart in flutter folder

--- flutter/main.dart
+++ flutter/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Tote',
+      title: Constants.appName,
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme(),
       darkTheme: AppTheme.darkTheme(),
