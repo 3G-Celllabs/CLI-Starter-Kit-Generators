7d0c6a8579c2d65a600cc7904e5f71aac822bafd
Pranay Prasad <pranay.prasad@celllabs.com>
2023-04-21 04:53:59 +0530

    Update /web/angular files.

--- web/angular/main.ts
+++ web/angular/main.ts
@@ -1,10 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode, importProvidersFrom } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
 
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { routes } from './app/app.routes';
+import { routes } from "./app/app.routes";
+import { AppComponent } from "./app/app.component";
+import { environment } from "./environments/environment";
+import { HttpClientModule } from "@angular/common/http";
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [importProvidersFrom(HttpClientModule), provideRouter(routes)],
