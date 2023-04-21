14e573730bc64e2a6ec4d0f0af44b40b5872131c
Pranay Prasad <pranay.prasad@celllabs.com>
2023-04-21 12:16:44 +0530

    Update /create-flutter-app-entity-model files

--- scripts/create-flutter-app-entity-model/main/end-script.js
+++ scripts/create-flutter-app-entity-model/main/end-script.js
@@ -5,11 +5,6 @@ exports.endScript = () => {
   console.log("\n");
   console.log(
     chalk.magentaBright.bgYellowBright.bold(
-      " Make sure to add assets/.env, assets/images to assets in pubspec.yaml "
-    )
-  );
-  console.log(
-    chalk.magentaBright.bgYellowBright.bold(
       " Few files need to be imported in main.dart, routes.dart "
     )
   );
